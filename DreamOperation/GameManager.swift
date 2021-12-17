c585ba7cf0675ef79dab39d9489cd42c3a3e9361
33 kazua <21cm0133@jec.ac.jp>
2021-12-17 17:48:12 +0900

    saicoro_create

--- DreamOperation/GameManager.swift
+++ DreamOperation/GameManager.swift
@@ -8,5 +8,9 @@
 import SwiftUI
 
 class GameManager: ObservableObject {
-    
+    /// サイコロロジック
+    func saicoro(max: Int) -> Int {
+        let saicoro = Int.random(in: 1...max)
+        return saicoro
+    }
 }

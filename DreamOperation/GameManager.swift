b6eb8f7f1dfc3bb026ffb5bfe49301f49e9926e4
33 kazua <21cm0133@jec.ac.jp>
2021-12-17 18:23:56 +0900

    bug

--- DreamOperation/GameManager.swift
+++ DreamOperation/GameManager.swift
@@ -13,7 +13,16 @@ class GameManager: ObservableObject {
     @Published var pins:[(num: Int, name: String, color: String, style: String,
                           place: Int, money: Int, health: Int,
                           salary: Int, pay: Int, follow1: String, follow2: String)] = []
-
+    
+    @Published var stepPosi: [(x:CGFloat, y: CGFloat)] = []
+    @Published var isPosi = false
+    
+    @Published var mapPosi = (x: CGFloat(0), y: CGFloat(0))
+    // stepの座標に合わせる
+    @Published var pin1 = (x: CGFloat(0), y: CGFloat(0))
+    @Published var pin2 = (x: CGFloat(0), y: CGFloat(0))
+    @Published var pinposi = (a: 0,b: 0,c: 0,d: 0)
+    
     /// サイコロロジック
     func saicoro(max: Int) -> Int {
         let saicoro = Int.random(in: 1...max)
--- DreamOperation/PlayView.swift
+++ DreamOperation/PlayView.swift
@@ -12,55 +12,48 @@ struct PlayView: View {
     let w = UIScreen.main.bounds.width
     let h = UIScreen.main.bounds.height
     
-    @State var stepPosi: [(x:CGFloat, y: CGFloat)] = []
-    @State var isPosi = false
-    
-    @State var mapPosi = (x: CGFloat(0), y: CGFloat(0))
-    // stepの座標に合わせる
-    @State var pin1 = (x: CGFloat(0), y: CGFloat(0))
-    @State var pin2 = (x: CGFloat(0), y: CGFloat(0))
-    @State var pinposi = (a: 0,b: 0,c: 0,d: 0)
+   
     var body: some View {
         ZStack{
             Group{
-                if isPosi{
+                if data.isPosi{
                     ForEach(0..<66){ num in
-                        box(posi: stepPosi[num], n: num)
+                        box(posi: data.stepPosi[num], n: num)
                     }
                     Image("pinImage")
                         .resizable()
                         .frame(width: 40, height: 80)
-                        .position(x: stepPosi[pinposi.a].x, y: stepPosi[pinposi.a].y-w/40)
+                        .position(x: data.stepPosi[data.pinposi.a].x, y: data.stepPosi[data.pinposi.a].y-w/40)
                     Image("pinImage")
                         .resizable()
                         .frame(width: 40, height: 80)
-                        .position(x: stepPosi[pinposi.b].x, y: stepPosi[pinposi.b].y-w/40)
+                        .position(x: data.stepPosi[data.pinposi.b].x, y: data.stepPosi[data.pinposi.b].y-w/40)
                 }
-            }.position(x: mapPosi.x, y: mapPosi.y)
+            }.position(x: data.mapPosi.x, y: data.mapPosi.y)
             HStack{
                 VStack{
-                    Button("↑"){mapPosi.y+=50}
+                    Button("↑"){data.mapPosi.y+=50}
                     HStack{
-                        Button("←"){mapPosi.x+=50}
-                        Button("○"){mapPosi=(x: w/2, y: h/2)}
-                        Button("→"){mapPosi.x-=50}
+                        Button("←"){data.mapPosi.x+=50}
+                        Button("○"){data.mapPosi=(x: w/2, y: h/2)}
+                        Button("→"){data.mapPosi.x-=50}
                     }
-                    Button("↓"){mapPosi.y-=50}
+                    Button("↓"){data.mapPosi.y-=50}
                 }
             
             VStack{
                 HStack{
-                    Button("←"){if pinposi.a>=1{pinposi.a-=1}}
+                    Button("←"){if data.pinposi.a>=1{data.pinposi.a-=1}}
                     Button("1"){}
-                    Button("→"){pinposi.a+=1}
+                    Button("→"){data.pinposi.a+=1}
                 }
             }
             
             VStack{
                 HStack{
-                    Button("←"){if pinposi.b>=1{pinposi.b-=1}}
+                    Button("←"){if data.pinposi.b>=1{data.pinposi.b-=1}}
                     Button("2"){}
-                    Button("→"){pinposi.b+=1}
+                    Button("→"){data.pinposi.b+=1}
                 }
             }
             }// HS
@@ -71,25 +64,25 @@ struct PlayView: View {
                 HStack{
                     VStack{
                         Text("マップ")
-                        Text("x:\(mapPosi.x) ")
-                        Text("y:\(mapPosi.y) ")
+                        Text("x:\(data.mapPosi.x) ")
+                        Text("y:\(data.mapPosi.y) ")
                     }
                     VStack{
                         Text("ピン１")
-                        Text("x:\(pin1.x) ")
-                        Text("y:\(pin1.y) ")
+                        Text("x:\(data.pin1.x) ")
+                        Text("y:\(data.pin1.y) ")
                     }
                     VStack{
                         Text("ピン２")
-                        Text("x:\(pin2.x) ")
-                        Text("y:\(pin2.y) ")
+                        Text("x:\(data.pin2.x) ")
+                        Text("y:\(data.pin2.y) ")
                     }
                     Spacer()
                 }
                 Spacer()
             }
         }.onAppear{
-            mapPosi = (x: w/2, y: -h)
+            data.mapPosi = (x: w/2, y: -h)
             createPosi()
         }
     }
@@ -97,30 +90,30 @@ struct PlayView: View {
         // 0~20
         for i in 0...20{
             let new = (x: w/10 * CGFloat(i), y: w)
-            stepPosi.append(new)
+            data.stepPosi.append(new)
         }
         // 21~30
         for i in 1...10{
             let new = (x: w * 2, y: w - w/10 * CGFloat(i))
-            stepPosi.append(new)
+            data.stepPosi.append(new)
         }
         //31~50
         for i in 1...20{
             let new = (x: w*2 - w/10 * CGFloat(i), y: CGFloat(0))
-            stepPosi.append(new)
+            data.stepPosi.append(new)
         }
         // 51~55
         for i in 1...5{
             let new = (x: CGFloat(0), y: w/10 * CGFloat(i))
-            stepPosi.append(new)
+            data.stepPosi.append(new)
         }
         // 56~65
         for i in 1...10{
             let new = (x: w/10 * CGFloat(i), y: w/2)
-            stepPosi.append(new)
+            data.stepPosi.append(new)
         }
         //mapPosi = (x: w/2, y: -h)
-        isPosi = true
+        data.isPosi = true
     }
 }
 

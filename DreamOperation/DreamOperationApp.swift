3aad1f00c7ab3bebb130b05260b6d496977ade9f
33kazua <21cm0133@jec.ac.jp>
2022-03-07 15:43:56 +0900

    トランクス死ね

--- DreamOperation/DreamOperationApp.swift
+++ DreamOperation/DreamOperationApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct DreamOperationApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             DisplayView()
